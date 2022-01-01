2a6d429d67ceec433d11a1c7b9eb3184c55457f4
Shubham Kushwaha <eric.liclair@gmail.com>
2022-01-01 23:01:25 +0530

    block swap - array rotation

---
+++ algos/blockSwap_ArrayRotation/blockSwap_ArrayRotation.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+void swapArr(int Arr[], int start, int end, int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        int temp = Arr[start + i];
+        Arr[start + i] = Arr[end + i];
+        Arr[end + i] = temp;
+    }
+}
+
+void blockSwap(int Arr[], int n, int k)
+{
+    if (k == n || k == 0)
+        return;
+
+    if (k == n - k)
+    // n(X) = n(Y)
+    {
+        swapArr(Arr, 0, n - k, k);
+        return;
+    }
+
+    if (k < n - k)
+    // n(X) < n(Y)
+    {
+        swapArr(Arr, 0, n - k, k);
+        blockSwap(Arr, n - k, k);
+    }
+    else
+    {
+        // k > n - k
+        // n(X) > n(Y)
+        swapArr(Arr, 0, k, n - k);
+        blockSwap(Arr + n - k, k, 2 * k - n);
+    }
+}
+
+int main()
+{
+    cout << "Algorithm for Array Rotation - Block Swap Method\nRef Link - https://www.geeksforgeeks.org/block-swap-algorithm-for-array-rotation/\n";
+    return 0;
+}
